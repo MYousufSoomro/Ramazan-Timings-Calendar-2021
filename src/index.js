99e013ac31a9f09692c1bd26d76fdc831ab1195b
M Yousuf Soomro <muhammad.yousuf1996@gmail.com>
2021-04-17 11:56:46 +0500

    PWA setup

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ import KarachiRamazanTimings from './pages/KarachiRamazanTimings';
 import RahimYarKhan from './pages/RahimYarKhan';
 import RYKRamazanTimings from './pages/RYKRamazanTimings';
 import ErrorPage from './pages/Error'
-import * as serviceWorker from './serviceWorker';
+import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 
 const routing = (
     <Router>
@@ -29,4 +29,4 @@ const routing = (
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.register();
+serviceWorkerRegistration.register();
